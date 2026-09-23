eac31c7c7fc88f67f7baed1919cc138a3fc59e8f
Jiac0726 <131017222+Jiac0726@users.noreply.github.com>
2026-09-23 17:56:12 +0800

    ui: expose generated backgrounds to pages

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,7 @@ Page({
   data: {
     dateStr: '', dayNum: '', yearMonth: '', weekDay: '', type: '', reason: '', submitted: false,
     loading: false, authLoading: true, bindingStatus: 'loading', canRate: false, partnerRating: null,
-    monthStreak: 0, monthGoodRate: 0, streakHearts: [0,0,0,0,0,0,0,0]
+    monthStreak: 0, monthGoodRate: 0, streakHearts: [0,0,0,0,0,0,0,0], reviewBg: app.globalData.REVIEW_BG
   },
   onLoad() {
     const now = new Date();
